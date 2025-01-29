f8b9872723b4ca5b27c533b095214f84a6ad04a0
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-01-29 09:57:54 -0700

    Added default

---
+++ src/components/Alert/presets/0-default.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Alert from '../Alert';
+
+export default (
+  <Alert header="Known issues/limitations" uxpId="alert-1">
+    Review the documentation to learn about potential compatibility issues with specific database versions.
+  </Alert>
+);
---
+++ src/components/Button/presets/0-default.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from '../Button';
+
+export default (
+  <Button variant="primary" uxpId="Button-1">
+    Click me
+  </Button>
+);
