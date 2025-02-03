5076153f31b8e9a053e7ab38464b9a066123c407
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-03 14:36:45 -0700

    Added theming

--- src/components/UXPinWrapper/UXPinWrapper.tsx
+++ src/components/UXPinWrapper/UXPinWrapper.tsx
@@ -1,6 +1,22 @@
-import { ReactNode } from 'react';
-import "@cloudscape-design/global-styles/index.css"
+import { ReactNode, useEffect } from 'react';
+import { Theme, applyTheme } from '@cloudscape-design/components/theming';
+import "@cloudscape-design/global-styles/index.css";
+
+const theme: Theme = {
+  //Edit your tokens here if needed
+  tokens: {
+    colorBackgroundButtonPrimaryDefault: {
+      light: '#006ce0',
+      dark: '#ff0000',
+    },
+  },
+};
 
 export default function UXPinWrapper({ children }: { children: ReactNode }) {
+
+  useEffect(() => {
+    applyTheme({ theme });
+  }, []);
+
   return children;
 }
