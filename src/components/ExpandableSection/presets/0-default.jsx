1f2825127e61f7489b1dc29342852827039a7b96
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-03 17:36:28 -0700

    Added preset

---
+++ src/components/ExpandableSection/presets/0-default.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import ExpandableSection from '../ExpandableSection';
+
+export default (
+  <ExpandableSection uxpId="expandable-section-1" headerText="Static website hosting">
+    After you enable your S3 bucket for static website hosting, web browsers can access your content through the Amazon
+    S3 website endpoint for the bucket.
+  </ExpandableSection>
+);
