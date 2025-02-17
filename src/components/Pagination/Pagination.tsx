45268e46483029befb7e961bd6118b1666453a83
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-17 16:15:04 -0700

    Added Pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import PaginationBase, { PaginationProps } from '@cloudscape-design/components/pagination';
+/**
+ * @uxpindocurl https://cloudscape.design/components/pagination/
+ * @uxpindescription Provides horizontal navigation between pages of a collection.
+ */
+export default (props: PaginationProps) => {
+  const [
+    currentPageIndex,
+    setCurrentPageIndex
+  ] = React.useState(1);
 
-const Pagination = (props:PaginationProps) => {
-  return <PaginationBase {...props} />;
-};
+  return (
+    <PaginationBase
+      {...props}
+      currentPageIndex={currentPageIndex}
+      onChange={({ detail }) =>
+        setCurrentPageIndex(detail.currentPageIndex)
+      }
+    />
+  );
+}
 
-export default Pagination;
---
+++ src/components/Pagination/presets/0-default.jsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import Pagination from '../Pagination';
+
+export default <Pagination uxpId="pagination-1" pagesCount={5} />;
