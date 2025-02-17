bd2dcb5d112ffc2fad5730a26a9b5f37aa4fd8d7
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-17 13:12:37 -0700

    Added FileInput

--- src/components/FileInput/FileInput.tsx
+++ src/components/FileInput/FileInput.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import FileInputBase, { FileInputProps } from '@cloudscape-design/components/file-input';
 
-const FileInput = (props:FileInputProps) => {
-  return <FileInputBase {...props} />;
+const FileInput = (props: FileInputProps) => {
+  // Ensure the initial state is mutable by spreading into a new array
+  const [value, setValue] = React.useState<File[]>([...(props.value || [])]);
+
+  React.useEffect(() => {
+    setValue([...(props.value || [])]); // Spread into a new array to ensure mutability
+  }, [props.value]); // Only depend on `props.value` instead of `props`
+
+  return (
+    <FileInputBase
+      {...props}
+      onChange={({ detail }) => setValue([...detail.value])} // Ensure a mutable array
+      value={value}
+    />
+  );
 };
 
 export default FileInput;
---
+++ src/components/FileInput/presets/0-default.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import FileInput from '../FileInput';
+
+export default (
+  <FileInput uxpId="file-input-1" multiple>
+    Choose file
+  </FileInput>
+);
