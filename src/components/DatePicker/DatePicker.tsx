3ddeed0160da71a60b0770737d7befc02c93b727
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-17 12:59:41 -0700

    Added DatePicker

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,8 +1,23 @@
+
 import React from 'react';
 import DatePickerBase, { DatePickerProps } from '@cloudscape-design/components/date-picker';
 
-const DatePicker = (props:DatePickerProps) => {
-  return <DatePickerBase {...props} />;
+const DatePicker = (props: DatePickerProps) => {
+  const [value, setValue] = React.useState(props.value);
+  React.useEffect(() => {
+    setValue(props.value)
+  }, [props]);
+
+  return (
+    <DatePickerBase
+      {...props}
+      onChange={({ detail }) =>
+        setValue(detail.value)
+      }
+      value={value}
+    />
+  );
 };
+
 
 export default DatePicker;
---
+++ src/components/DatePicker/presets/0-default.jsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import DatePicker from '../DatePicker';
+
+export default <DatePicker uxpId="date-picker-1" placeholder="YYYY/MM/DD" />;
