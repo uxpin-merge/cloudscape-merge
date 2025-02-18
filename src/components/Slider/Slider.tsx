00c44ae9fc6ad6e54ecaa621515f881f0e4ab012
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-17 17:16:56 -0700

    Added Slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import SliderBase, { SliderProps } from '@cloudscape-design/components/slider';
-
-const Slider = (props:SliderProps) => {
-  return <SliderBase {...props} />;
+/**
+ * @uxpindocurl https://cloudscape.design/components/slider/
+ * @uxpindescription A slider enables users to select a value within a defined range.
+ */
+const Slider = (props: SliderProps) => {
+  const [value, setValue] = React.useState(props.value);
+  React.useEffect(() => {
+    setValue(props.value)
+  }, [props]);
+  return (
+    <SliderBase
+      {...props}
+      onChange={({ detail }) => setValue(detail.value)}
+      value={value}
+      max={100}
+      min={0}
+    />
+  );
 };
 
 export default Slider;
---
+++ src/components/Slider/presets/0-default.jsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import Slider from '../Slider';
+
+export default <Slider value={50} max={100} min={0} uxpId="slider-1" />;
