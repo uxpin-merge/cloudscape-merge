9660bdb97c962e99a7af520bdfe1ff14f8c19a74
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-17 19:15:44 -0700

    Added LiveRegion

--- src/components/LiveRegion/LiveRegion.tsx
+++ src/components/LiveRegion/LiveRegion.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import LiveRegionBase, { LiveRegionProps } from '@cloudscape-design/components/live-region';
-
-const LiveRegion = (props:LiveRegionProps) => {
-  return <LiveRegionBase {...props} />;
+/**
+* @uxpindocurl https://cloudscape.design/components/live-region/
+* @uxpindescription A non-visual component used to announce page changes to assistive technology.
+*/
+const LiveRegion = (props: LiveRegionProps) => {
+  return <div><LiveRegionBase {...props} /></div>;
 };
 
 export default LiveRegion;
---
+++ src/components/LiveRegion/presets/0-default.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import LiveRegion from '../LiveRegion';
+
+export default (
+  <LiveRegion uxpId="live-region-1">
+    A non-visual component used to announce page changes to assistive technology.
+  </LiveRegion>
+);
