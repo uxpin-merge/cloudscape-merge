7f2b34ac5864cf5509df832fc956d1df38d7c9b9
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-17 18:12:42 -0700

    Added TextArea

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import TextareaBase, { TextareaProps } from '@cloudscape-design/components/textarea';
+/**
+* @uxpindocurl https://cloudscape.design/components/text-area/
+* @uxpindescription A form element that provides a multi-line, plain-text input control.
+*/
+const Textarea = (props: TextareaProps) => {
+  const [value, setValue] = React.useState(props.value);
+  React.useEffect(() => {
+    setValue(props.value)
+  }, [props]);
 
-const Textarea = (props:TextareaProps) => {
-  return <TextareaBase {...props} />;
+  return (
+    <TextareaBase
+      {...props}
+      onChange={({ detail }) => setValue(detail.value)}
+      value={value}
+    />
+  );
+
 };
 
 export default Textarea;
---
+++ src/components/Textarea/presets/0-default.jsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import Textarea from '../Textarea';
+
+export default <Textarea placeholder="This is a placeholder" uxpId="text-area-1" />;
