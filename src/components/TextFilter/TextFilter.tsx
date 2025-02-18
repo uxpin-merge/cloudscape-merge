026b07ed9b8b2a61dd0d3bfd66258718399b8e33
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-17 18:50:25 -0700

    Added TextFilter

--- src/components/TextFilter/TextFilter.tsx
+++ src/components/TextFilter/TextFilter.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import TextFilterBase, { TextFilterProps } from '@cloudscape-design/components/text-filter';
+/**
+* @uxpindocurl https://cloudscape.design/components/text-filter/
+* @uxpindescription With a text filter, users can enter text that’s used to match specific items in a collection.
+*/
+const TextFilter = (props: TextFilterProps) => {
+  const [
+    filteringText,
+    setFilteringText
+  ] = React.useState(props.filteringText);
 
-const TextFilter = (props:TextFilterProps) => {
-  return <TextFilterBase {...props} />;
+  React.useEffect(() => {
+    setFilteringText(props.filteringText)
+  }, [props]);
+
+  return <TextFilterBase
+    {...props}
+    filteringText={filteringText}
+    onChange={({ detail }) =>
+      setFilteringText(detail.filteringText)
+    }
+  />;
 };
 
 export default TextFilter;
---
+++ src/components/TextFilter/presets/0-default.jsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import TextFilter from '../TextFilter';
+
+export default <TextFilter filteringPlaceholder="Find instances" uxpId="text-filter-1" />;
