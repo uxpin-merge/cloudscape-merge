13c95ccb4c0b131fffaf5deefd002d582f0e8b50
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-17 17:33:56 -0700

    Added SplitPanel

--- src/components/SplitPanel/SplitPanel.tsx
+++ src/components/SplitPanel/SplitPanel.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import SplitPanelBase, { SplitPanelProps } from '@cloudscape-design/components/split-panel';
-
-const SplitPanel = (props:SplitPanelProps) => {
+/**
+ * @uxpindocurl https://cloudscape.design/components/status-indicator/
+ * @uxpindescription NOTE: Must be used inside AppLayoput. A collapsible panel that provides access to secondary information or controls. It is the primary component to implement split view, a pattern to display resource collection with contextual resource details.
+ */
+const SplitPanel = (props: SplitPanelProps) => {
   return <SplitPanelBase {...props} />;
 };
 
---
+++ src/components/SplitPanel/presets/0-default.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import SplitPanel from '../SplitPanel';
+
+export default (
+  <SplitPanel header="Split Panel Header" uxpId="split-panel-1">
+    Split Panel content
+  </SplitPanel>
+);
