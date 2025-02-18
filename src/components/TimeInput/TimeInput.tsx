94a909bb8481112ed0834ac07b77b27dc226b558
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-17 18:58:44 -0700

    Added TimeInput

--- src/components/TimeInput/TimeInput.tsx
+++ src/components/TimeInput/TimeInput.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import TimeInputBase, { TimeInputProps } from '@cloudscape-design/components/time-input';
-
-const TimeInput = (props:TimeInputProps) => {
-  return <TimeInputBase {...props} />;
+/**
+* @uxpindocurl https://cloudscape.design/components/time-input/
+* @uxpindescription A form element in which a user can enter an absolute time value.
+*/
+const TimeInput = (props: TimeInputProps) => {
+  const [value, setValue] = React.useState("");
+  React.useEffect(() => {
+    setValue(props.value)
+  }, [props]);
+  return <TimeInputBase {...props}
+    onChange={({ detail }) => setValue(detail.value)}
+    value={value}
+  />;
 };
 
 export default TimeInput;
---
+++ src/components/TimeInput/presets/0-default.jsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import TimeInput from '../TimeInput';
+
+export default <TimeInput format="hh:mm" placeholder="hh:mm" uxpId="time-input-1" />;
