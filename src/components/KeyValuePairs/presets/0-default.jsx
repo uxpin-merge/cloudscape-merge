5edbd3ea1d5a6ad108b0445a56a90b234e39cb04
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-20 16:19:23 -0700

    Cleanup

--- src/components/KeyValuePairs/presets/0-default.jsx
+++ src/components/KeyValuePairs/presets/0-default.jsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
 import KeyValuePairs from '../KeyValuePairs';
-import Link from '../../Link/Link';
-import CopyToClipboard from '../../CopyToClipboard/CopyToClipboard';
-import StatusIndicator from '../../StatusIndicator/StatusIndicator';
-import ProgressBar from '../../ProgressBar/ProgressBar';
 
 export default (
   <KeyValuePairs
@@ -13,41 +9,19 @@ export default (
       {
         label: 'Distribution ID',
         value: 'E1WG1ZNPRXT0D4',
-        info: (
-          <Link variant="info" href="#" uxpId="kvp-link-1">
-            Info
-          </Link>
-        ),
       },
       {
         label: 'ARN',
-        value: (
-          <CopyToClipboard
-            copyButtonAriaLabel="Copy ARN"
-            copyErrorText="ARN failed to copy"
-            copySuccessText="ARN copied"
-            textToCopy="arn:service23G24::111122223333:distribution/23E1WG1ZNPRXT0D4"
-            variant="inline"
-            uxpId="kvp-copy-to-clipboard-1"
-          />
-        ),
+        value: 'arn:service23G24::111122223333:distribution/23E1WG1ZNPRXT0D4',
       },
       {
         label: 'Status',
-        value: <StatusIndicator uxpId="kvp-status-indicator-1">Available</StatusIndicator>,
+        value: 'Available',
       },
       {
         label: 'SSL Certificate',
         id: 'ssl-certificate-id',
-        value: (
-          <ProgressBar
-            value={30}
-            additionalInfo="Additional information"
-            description="Progress bar description"
-            ariaLabelledby="ssl-certificate-id"
-            uxpId="kvp-progress-indicator-1"
-          />
-        ),
+        value: 30,
       },
       {
         label: 'Price class',
@@ -55,11 +29,7 @@ export default (
       },
       {
         label: 'CNAMEs',
-        value: (
-          <Link external={true} href="#" uxpId="kvp-link-2">
-            abc.service23G24.xyz
-          </Link>
-        ),
+        value: 'abc.service23G24.xyz',
       },
     ]}
   />
