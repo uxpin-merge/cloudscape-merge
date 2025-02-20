18d8b3cdffef97ba4a57bc6c9fc8b440608c0ac4
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-19 17:47:59 -0700

    Cleanup

--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -1,45 +1,19 @@
-// import React from "react";
-// import CodeEditorBase, { CodeEditorProps } from "@cloudscape-design/components/code-editor";
-// import ace from "ace-builds"; // Import ace for the 'ace' prop
-// import "./styles/theme/cloud_editor.css";
-// import "./styles/theme/cloud_editor_dark.css";
-// import "./styles/ace.css";
-// export default (props: CodeEditorProps) => {
-//   const [preferences, setPreferences] = React.useState<CodeEditorProps["preferences"]>(undefined);
-//   const [loading, setLoading] = React.useState(false);
-
-//   return (
-//     <CodeEditorBase
-//       {...props}
-//       ace={ace}
-//     // language="javascript"
-//     // value="const pi = 3.14;"
-//     // preferences={preferences}
-//     // onPreferencesChange={(e) => setPreferences(e.detail)}
-//     // loading={loading}
-//     // themes={{
-//     //   light: ["cloud_editor"],
-//     //   dark: ["cloud_editor_dark"],
-//     // }}
-
-//     />
-//   );
-// };
-
-
 import React, { useEffect, useState } from "react";
 import CodeEditorM, { CodeEditorProps } from "@cloudscape-design/components/code-editor";
+import ace from "ace-builds";
 import "./styles/theme/cloud_editor.css";
 import "./styles/theme/cloud_editor_dark.css";
 import "./styles/ace.css";
 
 
-import ace from "ace-builds";
 export function aceLoader() {
   return import("ace-builds").then((module) => module.default);
 }
 
-
+/**
+ * @uxpindocurl https://cloudscape.design/components/code-editor/
+ * @uxpindescription With the code editor, users can write and edit code.
+ */
 const i18nStrings: CodeEditorProps["i18nStrings"] = {
   loadingState: "Loading code editor",
   errorState: "There was an error loading the code editor.",
@@ -98,7 +72,6 @@ export default function CodeEditor(props: CodeEditorProps) {
       preferences={preferences}
       onPreferencesChange={(event) => setPreferences(event.detail)}
       i18nStrings={i18nStrings}
-      themes={{ light: ["cloud_editor"], dark: ["cloud_editor_dark"] }}
     />
   );
 }
--- src/components/CodeView/CodeView.tsx
+++ src/components/CodeView/CodeView.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import typescriptHighlight from "@cloudscape-design/code-view/code-view/highlight/typescript";
-
 import CodeView, { CodeViewProps } from "@cloudscape-design/code-view/code-view";
+/**
+ * @uxpindocurl https://cloudscape.design/components/code-view/
+ * @uxpindescription Allow users to read and copy a code snippet.
+ */
 
 export default (props: CodeViewProps) => {
   return (
