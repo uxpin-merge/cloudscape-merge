12ad846c26661b9c2a5f75b162c6b2b4d3453ac5
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-20 10:01:30 -0700

    Added PromptInput

--- src/components/PromptInput/PromptInput.tsx
+++ src/components/PromptInput/PromptInput.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import PromptInputBase, { PromptInputProps } from '@cloudscape-design/components/prompt-input';
+/**
+ * @uxpindocurl https://cloudscape.design/components/prompt-input/
+ * @uxpindescription Enables users to provide a prompt or command.
+ */
+const PromptInput = (props: PromptInputProps) => {
+  const [value, setValue] = React.useState(props.value);
 
-const PromptInput = (props:PromptInputProps) => {
-  return <PromptInputBase {...props} />;
+  React.useEffect(() => {
+    setValue(props.value);
+  }, [props.value]);
+
+  return (
+    <PromptInputBase
+      {...props}
+      onChange={({ detail }) => setValue(detail.value)}
+      value={value}
+    />
+  );
 };
 
 export default PromptInput;
---
+++ src/components/PromptInput/presets/0-default.jsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import PromptInput from '../PromptInput';
+
+export default <PromptInput uxpId="prompt-input-1" ariaLabel="Default prompt input" placeholder="Ask a question" />;
