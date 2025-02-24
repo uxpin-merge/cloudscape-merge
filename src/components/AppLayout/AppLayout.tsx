41c3b5cd156b586309df0ca73d38a0e786309068
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-24 16:06:37 -0700

    Fixed AppLayout toggles

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -5,7 +5,25 @@ import AppLayoutBase, { AppLayoutProps } from '@cloudscape-design/components/app
  * @uxpindescription Provides page structure for productive use cases, which include collapsible side navigation, tools panel, drawers, and split panel.
  */
 const AppLayout = (props: AppLayoutProps) => {
-  return <AppLayoutBase {...props} />;
+  const [isToolsOpen, setIsToolsOpen] = React.useState(props.toolsOpen);
+  const [isNavigationOpen, setIsNavigationOpen] = React.useState(props.navigationOpen);
+  const [isSplitPanelOpen, setSplitPanelOpen] = React.useState(props.splitPanelOpen);
+
+  React.useEffect(() => {
+    setIsToolsOpen(props.toolsOpen)
+    setIsNavigationOpen(props.navigationOpen)
+    setSplitPanelOpen(props.splitPanelOpen)
+  }, [props]);
+
+  return <AppLayoutBase
+    {...props}
+    navigationOpen={isNavigationOpen}
+    onNavigationChange={({ detail }) => setIsNavigationOpen(detail.open)}
+    toolsOpen={isToolsOpen}
+    onToolsChange={({ detail }) => setIsToolsOpen(detail.open)}
+    splitPanelOpen={isSplitPanelOpen}
+  />;
+
 };
 
 export default AppLayout;
