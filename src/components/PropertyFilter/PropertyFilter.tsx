316fef97eb432b36cbb4f4eefb57589653dcfc56
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-02-24 19:38:18 -0700

    Added PropertyFilter

--- src/components/PropertyFilter/PropertyFilter.tsx
+++ src/components/PropertyFilter/PropertyFilter.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import PropertyFilterBase, { PropertyFilterProps } from '@cloudscape-design/components/property-filter';
+import React, { useState } from "react";
+import PropertyFilterBase, { PropertyFilterProps } from "@cloudscape-design/components/property-filter";
+/**
+* @uxpindocurl https://cloudscape.design/components/property-filter/
+* @uxpindescription With the property filter, users can find specific items in a collection of resources by using properties, property values, typing free text, and combining these with operators.
+*/
+const PropertyFilter = (props: PropertyFilterProps) => {
+  const [query, setQuery] = useState<PropertyFilterProps["query"]>({
+    tokens: [],
+    operation: "and" as PropertyFilterProps["query"]["operation"],
+  });
 
-const PropertyFilter = (props:PropertyFilterProps) => {
-  return <PropertyFilterBase {...props} />;
+  return (
+    <PropertyFilterBase
+      {...props}
+      query={query}
+      onChange={({ detail }) => setQuery(detail)}
+    />
+  );
 };
 
 export default PropertyFilter;
