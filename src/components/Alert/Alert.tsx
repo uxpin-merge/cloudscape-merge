fb2d4fb7e21c1bf52a4bee076476c1a656b52cc0
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-03-14 14:08:42 -0600

    Added dismissable function

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import AlertBase, { AlertProps } from '@cloudscape-design/components/alert';
+import React from "react";
+import AlertBase, { AlertProps } from "@cloudscape-design/components/alert";
+
 /**
  * @uxpindocurl https://cloudscape.design/components/alert/
  * @uxpindescription A brief message that provides information or instructs users to take a specific action.
  */
+
 const Alert = (props: AlertProps) => {
-  return <AlertBase {...props} />;
+  // Ensure visible state is always a boolean
+  const [visible, setVisible] = React.useState<boolean>(props.visible ?? true);
+
+  React.useEffect(() => {
+    setVisible(props.visible ?? true); // Ensure it's always boolean
+  }, [props.visible]);
+
+  // Hide the alert when dismissed
+  const handleDismiss = () => {
+    setVisible(false);
+  };
+
+  // Do not render the Alert if not visible
+  if (!visible) return null;
+
+  return (
+    <AlertBase
+      {...props}
+      onDismiss={handleDismiss} // Close alert on clicking X
+      visible={visible}
+    />
+  );
 };
 
 export default Alert;
