c56f1592f730e4bccb26ae3b3983c44abfffc875
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-03-14 14:25:51 -0600

    fixed activeTabId state

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -5,7 +5,19 @@ import TabsBase, { TabsProps } from '@cloudscape-design/components/tabs';
  * @uxpindescription With tabs, users can switch between different categories of information in the same view.
  */
 const Tabs = (props: TabsProps) => {
-  return <TabsBase {...props} />;
+  const [activeTabId, setActiveTabId] = React.useState(
+    props.activeTabId
+  );
+  React.useEffect(() => {
+    setActiveTabId(props.activeTabId)
+  }, [props]); // Only re-run the effect if value prop changes
+
+  return <TabsBase {...props}
+    onChange={({ detail }) =>
+      setActiveTabId(detail.activeTabId)
+    }
+    activeTabId={activeTabId}
+  />;
 };
 
 export default Tabs;
