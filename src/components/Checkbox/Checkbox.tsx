de2b0f31727534f8b63f40e389f9203c975136e0
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-03-19 09:11:26 -0600

    uxpinOnChange prop added

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -4,17 +4,18 @@ import CheckboxBase, { CheckboxProps } from '@cloudscape-design/components/check
  * @uxpindocurl https://cloudscape.design/components/checkbox/
  * @uxpindescription Checkboxes enable users to turn an option on or off.
  */
-const Checkbox = (props: CheckboxProps) => {
-  const [checked, setChecked] = React.useState(props.checked);
-  React.useEffect(() => {
-    setChecked(props.checked)
-  }, [props]); // Only re-run the effect if value prop changes
-
+const Checkbox = ({ checked = false, ...props }: CheckboxProps) => {
   return (
     <CheckboxBase
       {...props}
-      onChange={({ detail }) =>
-        setChecked(detail.checked)
+      onChange={(e) => {
+        // uxpinOnChange(prevValue, nextValue, propertyName) property injected by uxpin
+        // @ts-ignore
+        props.uxpinOnChange(checked, e.detail.checked, 'checked');
+        if (props.onChange) {
+          props.onChange(e);
+        }
+      }
       }
       checked={checked}
     />
