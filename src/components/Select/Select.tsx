2d973b7ea88a72244b0025141125a8b3714d08a5
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-03-26 11:27:00 -0600

    Added onChange value binding

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -4,19 +4,20 @@ import SelectBase, { SelectProps } from '@cloudscape-design/components/select';
 * @uxpindocurl https://cloudscape.design/components/select/
 * @uxpindescription Selects enable users to choose a single item from a list of items.
 */
-const Select = (props: SelectProps) => {
-  const [selectedOption, setSelectedOption] = React.useState(props.selectedOption);
-  React.useEffect(() => {
-    setSelectedOption(props.selectedOption)
-  }, [props]);
+interface ExtendedSelectProps extends SelectProps {
+  /**
+   *  ID of the selected option. If you want to clear the selection, use null.
+  * @uxpinbind onChange 0.detail.selectedOption
+  */
+  selectedOption: any;
+}
+
+const Select = (props: ExtendedSelectProps) => {
 
   return (
     <SelectBase
       {...props}
-      onChange={({ detail }) =>
-        setSelectedOption(detail.selectedOption)
-      }
-      selectedOption={selectedOption}
+
     />
   );
 };
