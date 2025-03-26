2c86b3354168894fac4929e786c9f4fd1a750af1
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-03-26 11:52:30 -0600

    Added @uxpinbind

--- src/components/RadioGroup/RadioGroup.tsx
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import RadioGroupBase, { RadioGroupProps } from '@cloudscape-design/components/radio-group';
+
+interface ExtendedRadioGroupProps extends RadioGroupProps {
+  /**
+   *  Value of the input.
+  * @uxpinbind onChange 0.detail.value
+  */
+  value: string;
+}
+
+
 /**
  * @uxpindocurl https://cloudscape.design/components/radio-group/
  * @uxpindescription Radio group enable users to choose one option from a predefined set.
  */
-const RadioGroup = (props: RadioGroupProps) => {
-  const [value, setValue] = React.useState(props.value);
-  React.useEffect(() => {
-    setValue(props.value)
-  }, [props]);
-
-  return <RadioGroupBase {...props} onChange={({ detail }) => setValue(detail.value)}
-    value={value}
-  />;
+const RadioGroup = (props: ExtendedRadioGroupProps) => {
+  return <RadioGroupBase {...props} />;
 };
 
 export default RadioGroup;
