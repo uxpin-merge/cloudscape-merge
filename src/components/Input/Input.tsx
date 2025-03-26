1d5c6c13a2f015a6516dc969419fa54762d3dc28
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-03-26 11:44:47 -0600

    Added @uxpinbind

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import InputBase, { InputProps } from '@cloudscape-design/components/input';
+interface ExtendedInputProps extends InputProps {
+  /**
+   *  ID of the selected option. If you want to clear the selection, use null.
+  * @uxpinbind onChange 0.detail.value
+  */
+  value: string;
+}
 /**
  * @uxpindocurl https://cloudscape.design/components/input/
  * @uxpindescription With the input control, users can input a single line of text.
  */
-const Input = (props: InputProps) => {
-  const [value, setValue] = React.useState(props.value);
-  React.useEffect(() => {
-    setValue(props.value)
-  }, [props]);
+const Input = (props: ExtendedInputProps) => {
 
   return (
     <InputBase
       {...props}
-      onChange={({ detail }) =>
-        setValue(detail.value)
-      }
-      value={value}
     />
   );
 };
