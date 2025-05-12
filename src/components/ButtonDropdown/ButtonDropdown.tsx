a762b63dddf1126b51485aeae4b665cab7b3945b
Robert Hilscher <r.hilscher@gmail.com>
2025-05-12 11:52:36 +0200

    add selectedItemId

--- src/components/ButtonDropdown/ButtonDropdown.tsx
+++ src/components/ButtonDropdown/ButtonDropdown.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ButtonDropdownBase, { ButtonDropdownProps } from '@cloudscape-design/components/button-dropdown';
+
+
+interface ExtendedButtonDropdownProps extends ButtonDropdownProps {
+  /**
+   *
+   *
+   * @uxpinbind onItemClick 0.detail.id
+   */
+  selectedItemId?:string;
+}
+
+
 /**
  * @uxpindocurl https://cloudscape.design/components/button-dropdown/
  * @uxpindescription With a button dropdown, you can group a set of actions under one button.
  */
-const ButtonDropdown = (props: ButtonDropdownProps) => {
-  return <ButtonDropdownBase {...props} />;
+const ButtonDropdown = ({ selectedItemId, ...otherProps}: ExtendedButtonDropdownProps) => {
+  return <ButtonDropdownBase {...otherProps} />;
 };
 
 export default ButtonDropdown;
