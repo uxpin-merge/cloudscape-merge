bebdf01633a6170e2e9e61f13b361f50c0ad7987
Robert Hilscher <r.hilscher@gmail.com>
2025-06-23 12:29:34 +0200

    handle custom styles for TopNavigation component

--- src/components/TopNavigation/TopNavigation.tsx
+++ src/components/TopNavigation/TopNavigation.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import TopNavigationBase, { TopNavigationProps } from '@cloudscape-design/components/top-navigation';
+import { useRef, useEffect } from 'react';
 /**
 * @uxpindocurl https://cloudscape.design/components/top-navigation/
 * @uxpindescription A global navigation element for services that is consistent and persists across all service pages.
 */
 const TopNavigation = (props: TopNavigationProps) => {
-  return <TopNavigationBase {...props} />;
+  const anchor = useRef(null); // initial value is null
+  //@ts-ignore
+  const style = props.style || null;
+
+  useEffect(() => {
+    if (anchor.current) {
+     const component = (anchor.current as HTMLElement).previousElementSibling;
+     if (component) {
+       const header = component.querySelector('header');
+       if (header) {
+         header.removeAttribute("style");
+         if (style) {
+           Object.keys(style).forEach((property) => {
+             // @ts-ignore
+             if (property.indexOf('--') === 0) {
+                header.style.setProperty(
+                 '--color-background-container-content-z79u4u',
+                  style[property]
+               );
+             } else {
+               header.style[property] = style[property];
+             }
+           });
+         }
+       }
+     }
+    }
+  }, [style]);
+
+  return (<>
+    <TopNavigationBase {...props} />
+    <span ref={anchor} />
+  </>)
 };
 
 export default TopNavigation;
