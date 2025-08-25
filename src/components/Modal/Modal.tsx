f737ced46eac21cf67c4e795498b8ef3f19e13c1
Robert Hilscher <r.hilscher@gmail.com>
2025-08-25 09:24:06 +0200

    handle onDismiss property in modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -14,7 +14,7 @@ export interface ExtendedModalProps extends ModalProps {
  * @uxpindescription A user interface element subordinate to an application's main window. It prevents interaction with the main page content, but keeps it visible with the modal as a child window in front of it.
  */
 const Modal = (props: ExtendedModalProps) => {
-  const [visible, setVisible] = React.useState(true);
+  const [visible, setVisible] = React.useState(props.visible);
 
   React.useEffect(() => {
     setVisible(props.visible)
@@ -38,7 +38,15 @@ const Modal = (props: ExtendedModalProps) => {
     <ModalBase
       {...props}
       footer={modifiedFooter}
-      // onDismiss={() => setVisible(false)}
+      onDismiss={(e) => {
+        // @ts-ignore
+        props.uxpinOnChange(true, false, 'visible');
+
+        if (props.onDismiss) {
+          props.onDismiss(e);
+        }
+
+      }}
       visible={visible}
     />
   )
